8835fc86737ed3355cf6d272edb838a8054a9e05
NPHCDA <NPHCDA@DESKTOP-EBEESKK>
2023-06-07 13:53:52 -0700

    Add project files.

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿/* the start of the program
+This is to output Hello,World
+using console app */
+Console.WriteLine("Hello, DDOCPs");
+
+
+//Declaration of variables
+string studentName;
+int studentAge;
+bool studentPresent;
+char studentSex;
+double studentFee;
+
+//Initialisation or assigning values to the variables
+studentName = "Kelvin";
+studentAge = 20;
+studentPresent = true;
+studentSex = 'M';
+studentFee = 1000000.60;
+
+// Basics operations with the variables(+,-,/,%)
+//Addition
+//int Age = 23 + studentAge;
+//studentAge = 23 + studentAge;
+studentAge += 23;
+Console.WriteLine("The new age of the student is, " + studentAge +" Years!!!") ;
+Console.ReadLine();
+
